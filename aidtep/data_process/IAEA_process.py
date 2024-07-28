2eeaae8cebd0d5469a0c6d55628572e8124bcb35
YannLee <chill_yann@sjtu.edu.cn>
2024-07-28 12:23:40 +0800

    update: use builder model to process data

--- aidtep/data_process/IAEA_process.py
+++ aidtep/data_process/IAEA_process.py
@@ -1,85 +1,105 @@
-import os
-from typing import Literal
-
-import numpy as np
-from loguru import logger
-
 from aidtep.data_process.common import down_sample_3d_data, extract_observations
+from aidtep.data_process.noise import add_noise
 from aidtep.data_process.sensor_position import generate_2d_specific_mask
 from aidtep.data_process.interpolation import voronoi_tessellation
+from aidtep.data_process.vibration import generate_vibrated_masks
 from aidtep.utils import constants
-from aidtep.utils.file import save_ndarray
+from aidtep.utils.file import save_ndarray, check_file_exist
+
+import numpy as np
+from typing import Literal
+from loguru import logger
 
 
-def load_IAEA_raw_data(data_path: str, data_size: int = constants.IAEA_DATA_SIZE, data_shape: tuple = constants.IAEA_DATA_SHAPE,
-                       data_type: str = "float16", ) -> np.ndarray:
-    """
-    Read IAEA raw data, reshape and convert data type. The data is stored in a text file, shape is (data_size * data_shape[0], data_shape[1]).
-    :param data_path: IAEA raw data path
-    :param data_size: Number of data
-    :param data_shape: data shape, example: (171, 171)
-    :param data_type: data type, default: float16
-    :return: IAEA raw data
-    """
-    data = np.loadtxt(data_path)
-    data = data.astype(data_type)
-    data = data.reshape(data_size, *data_shape)
-    return data
+class IAEADataProcessBuilder:
+    def __init__(self, obs_output_path: str, interpolation_output_path: str):
+        self.obs_output_path = obs_output_path
+        self.interpolation_output_path = interpolation_output_path
+        self.data = None
+        self.observation = None
+        self.position_mask = None
+        self.interpolation = None
 
+    def load_raw_data(self, data_path: str, data_type: str):
+        """
+        Load IAEA raw data from txt file.
+        :param data_path: str, path to the txt file
+        :param data_type: str, data type of the raw data
+        :return: Self
+        """
+        data = np.loadtxt(data_path)
+        data = data.astype(data_type)
+        self.data = data.reshape(constants.IAEA_DATA_SIZE, *constants.IAEA_DATA_SHAPE)
+        logger.debug(f"IAEA raw data shape: {self.data.shape}")
+        return self
 
-def main_process(data_path: str, data_type: str , down_sample_factor: int, down_sample_strategy: Literal["min", "max", "mean"],
-                 x_sensor_position: list, y_sensor_position: list, random_range: int, noise_ratio: float,
-                 obs_output_path: str, tessellation_output_path: str) :
-    """
-    Main process of IAEA raw data.
-    :param noise_ratio:
-    :param random_range:
-    :param obs_output_path:
-    :param x_sensor_position:
-    :param y_sensor_position:
-    :param data_path: IAEA raw data path
-    :param data_type: data type, default: float16
-    :param down_sample_factor: down sample factor, default: 1
-    :param down_sample_strategy: down sample strategy, default: "mean"
-    """
-    obs_output_path = obs_output_path.format(
-        data_type=data_type, down_sample_factor=down_sample_factor, down_sample_strategy=down_sample_strategy,
-        x_sensor_position=x_sensor_position, y_sensor_position=y_sensor_position, random_range=random_range,
-        noise_ratio=noise_ratio
-    )
-    tessellation_output_path = tessellation_output_path.format(
-        data_type=data_type, down_sample_factor=down_sample_factor, down_sample_strategy=down_sample_strategy,
-        x_sensor_position=x_sensor_position, y_sensor_position=y_sensor_position, random_range=random_range,
-        noise_ratio=noise_ratio
-    )
+    def down_sample_raw_data(self, down_sample_factor: int, down_sample_strategy: Literal["min", "max", "mean"]):
+        """
+        Down sample the raw data by a factor.
+        For example, if the raw data shape is (100, 100, 100), and down sample
+            factor is 2, the down sampled data shape will be (100, 50, 50).
+        :param down_sample_factor: int, down sample factor
+        :param down_sample_strategy: literal["min", "max", "mean"], down sample strategy
+        :return: Self
+        """
+        if self.data is not None:
+            self.data = down_sample_3d_data(self.data, down_sample_factor=down_sample_factor,
+                                            down_sample_strategy=down_sample_strategy)
+            logger.debug(f"After down sample, IAEA raw data shape: {self.data.shape}")
+        else:
+            raise ValueError("Please load raw data first.")
+        return self
 
-    import matplotlib.pyplot as plt
+    # TODO: support other type of sensor position
+    def get_observation(self, x_sensor_position: list, y_sensor_position: list, random_range: int, noise_ratio: float):
+        """
+        Generate observation data based on the sensor position.
+        :param x_sensor_position: list, x position of the sensor
+        :param y_sensor_position: list, y position of the sensor
+        :param random_range: int, random range for vibration
+        :param noise_ratio: float, noise ratio
+        :return: Self
+        """
+        if self.data is not None:
+            self.position_mask = generate_2d_specific_mask(self.data.shape[1], self.data.shape[2], x_sensor_position,
+                                                           y_sensor_position)
+            if random_range > 0:
+                vibration_position_mask = generate_vibrated_masks(self.position_mask, random_range=random_range,
+                                                                  n=self.data.shape[0])
+                self.observation = extract_observations(self.data, vibration_position_mask)
+            else:
+                self.observation = extract_observations(self.data, self.position_mask)
 
-    logger.info(f"obs_output_path: {obs_output_path}")
+            if noise_ratio > 0:
+                self.observation = add_noise(self.observation, noise_ratio)
 
-    # data = load_IAEA_raw_data(data_path=data_path, data_type=data_type)
-    #
-    # logger.info(f"IAEA raw data shape: {data.shape}")
-    # data = down_sample_3d_data(data, down_sample_factor=down_sample_factor, down_sample_strategy=down_sample_strategy)
-    # logger.info(f"After down sample, IAEA raw data shape: {data.shape}")
-    #
-    # plt.imshow(data[0])
-    # plt.savefig("IAEA_raw_data.png")
+            logger.debug(f"Observation shape: {self.observation.shape}")
+            save_ndarray(file_path=self.obs_output_path, data=self.observation)
+        else:
+            raise ValueError("Please load raw data first.")
+        return self
 
-    # position_mask = generate_2d_specific_mask(data.shape[0], data.shape[1], x_sensor_position, y_sensor_position)
-    # observation = extract_observations(data, position_mask)
-    #
-    # observation = add_noise(observation, noise_ratio)
-    # logger.info(f"Observation shape: {observation.shape}")
-    #
-    # save_ndarray(file_path=obs_output_path, data=observation)
+    # TODO: Add tessellation type
+    def interpolate(self, x_shape: int, y_shape: int, x_sensor_position: list, y_sensor_position: list,
+                    tessellation_type: str):
+        """
+        Interpolate the observation data based on the sensor position.
+        :param x_shape: int, x shape of the interpolation
+        :param y_shape: int, y shape of the interpolation
+        :param x_sensor_position: list, x position of the sensor
+        :param y_sensor_position: list, y position of the sensor
+        :param tessellation_type: str, tessellation type
+        :return: Self
+        """
+        if self.observation is not None:
+            if not check_file_exist(self.obs_output_path):
+                raise ValueError("Please generate observation first.")
+            self.observation = np.load(self.obs_output_path)
 
-    observation = np.load(obs_output_path)
-    position_mask = generate_2d_specific_mask(86, 86, x_sensor_position, y_sensor_position)
-    tessellation =  voronoi_tessellation(observation[:10], position_mask)
+        self.position_mask = generate_2d_specific_mask(x_shape, y_shape, x_sensor_position, y_sensor_position)
+        self.interpolation = voronoi_tessellation(self.observation, self.position_mask)
+        logger.debug(f"After interpolation, tessellation shape: {self.interpolation.shape}")
+        save_ndarray(file_path=self.interpolation_output_path, data=self.interpolation)
+        logger.debug(f"Interpolation saved to {self.interpolation_output_path}")
 
-    logger.info(f"tessellation shape: {tessellation.shape}")
-    save_ndarray(file_path=tessellation_output_path, data=tessellation)
-
-    plt.imshow(tessellation[0])
-    plt.savefig("tessellation.png")
+        return self
