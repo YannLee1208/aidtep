2a6b3f1cce4b75895b615a9463d186202d842d1e
YannLee <chill_yann@sjtu.edu.cn>
2024-07-28 15:33:37 +0800

    update: add true data process. And refactor config

--- aidtep/data_process/IAEA_process.py
+++ aidtep/data_process/IAEA_process.py
@@ -20,18 +20,21 @@ class IAEADataProcessBuilder:
         self.position_mask = None
         self.interpolation = None
 
-    def load_raw_data(self, data_path: str, data_type: str):
+    @staticmethod
+    def load_raw_data(data_path: str, data_type: str) -> np.ndarray:
         """
         Load IAEA raw data from txt file.
         :param data_path: str, path to the txt file
         :param data_type: str, data type of the raw data
-        :return: Self
+        :return: np.ndarray, IAEA raw data
         """
+        if not check_file_exist(data_path):
+            raise FileNotFoundError(f"File {data_path} not found.")
         data = np.loadtxt(data_path)
         data = data.astype(data_type)
-        self.data = data.reshape(constants.IAEA_DATA_SIZE, *constants.IAEA_DATA_SHAPE)
-        logger.debug(f"IAEA raw data shape: {self.data.shape}")
-        return self
+        data = data.reshape(constants.IAEA_DATA_SIZE, *constants.IAEA_DATA_SHAPE)
+        logger.debug(f"IAEA raw data shape: {data.shape}")
+        return data
 
     def down_sample_raw_data(self, down_sample_factor: int, down_sample_strategy: Literal["min", "max", "mean"]):
         """
@@ -50,8 +53,33 @@ class IAEADataProcessBuilder:
             raise ValueError("Please load raw data first.")
         return self
 
+    def get_true_date(self, phione_input_path: str, phione_output_path: str, phitwo_input_path: str,
+                      phitwo_output_path: str,
+                      power_input_path: str, power_output_path: str,
+                      data_type: str,
+                      down_sample_factor: int, down_sample_strategy: Literal["min", "max", "mean"]):
+        phione = self.load_raw_data(phione_input_path, data_type)
+        phione = down_sample_3d_data(phione, down_sample_factor, down_sample_strategy)
+        save_ndarray(phione_output_path, phione)
+        logger.debug(f"Save phione to {phione_output_path}, data shape: {phione.shape}")
+        del phione
+
+        phitwo = self.load_raw_data(phitwo_input_path, data_type)
+        phitwo = down_sample_3d_data(phitwo, down_sample_factor, down_sample_strategy)
+        save_ndarray(phitwo_output_path, phitwo)
+        logger.debug(f"Save phitwo to {phitwo_output_path}, data shape: {phitwo.shape}")
+        del phitwo
+
+        power = self.load_raw_data(power_input_path, data_type)
+        power = down_sample_3d_data(power, down_sample_factor, down_sample_strategy)
+        save_ndarray(power_output_path, power)
+        logger.debug(f"Save power to {power_output_path}, data shape: {power.shape}")
+        del power
+
     # TODO: support other type of sensor position
-    def get_observation(self, x_sensor_position: list, y_sensor_position: list, random_range: int, noise_ratio: float):
+    def get_observation(self, data_path: str, data_type: str,
+                        down_sample_factor: int, down_sample_strategy: Literal["min", "max", "mean"],
+                        x_sensor_position: list, y_sensor_position: list, random_range: int, noise_ratio: float):
         """
         Generate observation data based on the sensor position.
         :param x_sensor_position: list, x position of the sensor
@@ -60,6 +88,8 @@ class IAEADataProcessBuilder:
         :param noise_ratio: float, noise ratio
         :return: Self
         """
+        self.data = self.load_raw_data(data_path, data_type)
+        self.data = down_sample_3d_data(self.data, down_sample_factor=down_sample_factor, down_sample_strategy=down_sample_strategy)
         if self.data is not None:
             self.position_mask = generate_2d_specific_mask(self.data.shape[1], self.data.shape[2], x_sensor_position,
                                                            y_sensor_position)
--- aidtep/launcher/data_process.py
+++ aidtep/launcher/data_process.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from loguru import logger
 
@@ -5,44 +6,57 @@ from aidtep.utils.initialize import initialize
 from aidtep.utils.config import AidtepConfig
 from aidtep.data_process.IAEA_process import IAEADataProcessBuilder
 
-
 if __name__ == '__main__':
     config_path = os.path.join(os.path.dirname(__file__), "..", "..", 'config', 'dev.yaml')
-    initialize()
+    initialize(log_level=logging.DEBUG)
     config = AidtepConfig(config_path)
     config_data_process = config.get("data_process")
-    for dataset_name  in config_data_process.keys():
+    for dataset_name in config_data_process.keys():
         dataset_config_map = config_data_process.get(dataset_name)
         if dataset_config_map.get("use"):
-            obs_output_path= dataset_config_map.get("output.obs_path")
+            # common config
+            obs_output_path = dataset_config_map.get("output.obs_path")
             interpolation_path = dataset_config_map.get("output.interpolation_path")
-            data_type = dataset_config_map.get("process.observation.data_type")
-            x_sensor_position = dataset_config_map.get("process.observation.x_sensor_position")
-            y_sensor_position = dataset_config_map.get("process.observation.y_sensor_position")
-            down_sample_factor = dataset_config_map.get("process.observation.down_sample.down_sample_factor")
-            down_sample_strategy = dataset_config_map.get("process.observation.down_sample.down_sample_strategy")
+            data_type = dataset_config_map.get("process.data_type")
+            x_sensor_position = dataset_config_map.get("process.x_sensor_position")
+            y_sensor_position = dataset_config_map.get("process.y_sensor_position")
+            down_sample_factor = dataset_config_map.get("process.down_sample_factor")
+            down_sample_strategy = dataset_config_map.get("process.down_sample_strategy")
 
             random_range = dataset_config_map.get("process.observation.vibration.random_range")
             noise_ratio = dataset_config_map.get("process.observation.noise.noise_ratio")
             interp_method = dataset_config_map.get("process.interpolation.method")
             interp_shape = dataset_config_map.get("process.interpolation.interpolation_shape")
 
-            obs_output_path = obs_output_path.format(data_type=data_type, down_sample_factor=down_sample_factor, down_sample_strategy=down_sample_strategy, x_sensor_position=x_sensor_position, y_sensor_position=y_sensor_position, random_range=random_range, noise_ratio=noise_ratio)
-            interpolation_path = interpolation_path.format(method=interp_method, data_type=data_type, down_sample_factor=down_sample_factor, down_sample_strategy=down_sample_strategy, x_sensor_position=x_sensor_position, y_sensor_position=y_sensor_position, random_range=random_range, noise_ratio=noise_ratio)
+            obs_output_path = obs_output_path.format(data_type=data_type, down_sample_factor=down_sample_factor,
+                                                     down_sample_strategy=down_sample_strategy,
+                                                     x_sensor_position=x_sensor_position,
+                                                     y_sensor_position=y_sensor_position, random_range=random_range,
+                                                     noise_ratio=noise_ratio)
+            interpolation_path = interpolation_path.format(method=interp_method, data_type=data_type,
+                                                           down_sample_factor=down_sample_factor,
+                                                           down_sample_strategy=down_sample_strategy,
+                                                           x_sensor_position=x_sensor_position,
+                                                           y_sensor_position=y_sensor_position,
+                                                           random_range=random_range, noise_ratio=noise_ratio)
 
             IA = IAEADataProcessBuilder(obs_output_path, interpolation_path)
+            if dataset_config_map.get("process.true_data.use"):
+                IA.get_true_date(
+                    dataset_config_map.get("input.phione_path"),
+                    dataset_config_map.get("output.phione_path"),
+                    dataset_config_map.get("input.phitwo_path"), dataset_config_map.get("output.phitwo_path"),
+                    dataset_config_map.get("input.power_path"), dataset_config_map.get("output.power_path"),
+                    data_type, down_sample_factor, down_sample_strategy)
+
             if dataset_config_map.get("process.observation.use"):
                 logger.info(f"start process {dataset_name} observation data")
-                IA.load_raw_data(dataset_config_map.get("input.obs_path"), data_type)
-                if dataset_config_map.get("process.observation.down_sample.use"):
-                    logger.info(f"data down sample factor: {down_sample_factor}, strategy: {down_sample_strategy}")
-                    IA.down_sample_raw_data(down_sample_factor, down_sample_strategy)
                 if not dataset_config_map.get("process.observation.vibration.use"):
                     random_range = 0
                 if not dataset_config_map.get("process.observation.noise.use"):
                     noise_ratio = 0
                 logger.info(f"vibration random range: {random_range}, noise ratio: {noise_ratio}")
-                IA.get_observation(x_sensor_position, y_sensor_position, random_range, noise_ratio)
+                IA.get_observation(dataset_config_map.get("input.obs_path"), data_type, down_sample_factor, down_sample_strategy,  x_sensor_position, y_sensor_position, random_range, noise_ratio)
 
             if dataset_config_map.get("process.interpolation.use"):
                 logger.info(f"start process {dataset_name} interpolation data")
