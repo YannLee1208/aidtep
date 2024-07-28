feb9cb24e568d0ece747c596fea718c7152508a8
YannLee <chill_yann@sjtu.edu.cn>
2024-07-28 12:17:45 +0800

    update: remove initialize config, use specific initialization

--- aidtep/utils/initialize.py
+++ aidtep/utils/initialize.py
@@ -1,9 +1,7 @@
 import logging
 
 from aidtep.utils.logger import init_logger
-from aidtep.utils.config import init_config
 
 
-def initialize(config_path:str,   log_level=logging.INFO):
+def initialize( log_level=logging.INFO):
     init_logger(log_level)
-    init_config(config_path)
