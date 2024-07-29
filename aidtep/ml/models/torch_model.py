50c27963f783f69ebbf16f445b67a8355530dd1f
YannLee <chill_yann@sjtu.edu.cn>
2024-07-29 23:06:49 +0800

    update: add training process

--- aidtep/ml/models/torch_model.py
+++ aidtep/ml/models/torch_model.py
@@ -1,12 +1,12 @@
-import logging
-from typing import Literal
+from typing import Literal, Optional
 import torch
-from black import Optional
 from loguru import logger
 from torch import nn
 from torch.utils.data import DataLoader
 
 from aidtep.ml.models.base_model import BaseModel
+from aidtep.ml.utils.log_utils import ProgressLogger
+from aidtep.utils.file import make_parent_dir, check_file_exist
 
 
 class PyTorchModel(BaseModel):
@@ -34,34 +34,44 @@ class PyTorchModel(BaseModel):
         self.criterion = criterion.to(self.device)
         self.optimizer = optimizer
 
-        self.addtional_criterias = []
+        self.additional_criterions = {}
+        self.progress_logger = ProgressLogger()
 
-    def add_criteria(self, criterion):
-        self.addtional_criterias.append(criterion)
+    def add_criteria(self, name, criterion):
+        self.additional_criterions[name] = criterion
 
-    def train(self, dataloader: DataLoader, **kwargs) -> float:
+    def train(self, dataloader: DataLoader, progress_prefix: str = "", **kwargs) -> float:
         """
         Train the model for one epoch.
+        :param progress_prefix: str, prefix for the progress logger
         :param dataloader: DataLoader, containing training data
-        :param kwargs:
-        :return: epoch_loss
+        :return: total loss of dataloader
         """
         self.model.train()
         epoch_loss = 0.0
-        for inputs, targets in dataloader:
+        batch_number = len(dataloader)
+
+        for idx, (inputs, targets) in enumerate(dataloader):
             inputs, targets = inputs.to(self.device), targets.to(self.device)
             self.optimizer.zero_grad()
             outputs = self.model(inputs)
             loss = self.criterion(outputs, targets)
             loss.backward()
             self.optimizer.step()
-            if self.scheduler is not None:
-                self.scheduler.step()
             epoch_loss += loss.item()
+
+            with torch.no_grad():
+                criterion_losses = {"train_loss": loss.item()}
+                for name, criterion in self.additional_criterions.items():
+                    criterion_losses[name] = criterion(outputs, targets).item()
+                self.progress_logger.log_batch(progress_prefix, idx + 1, batch_number, criterion_losses)
+        self.progress_logger.finalize_log()
+
+        if self.scheduler is not None:
+            self.scheduler.step()
         return epoch_loss
 
     def predict(self, dataloader: DataLoader) -> torch.Tensor:
-        self.logger.info("Predicting with PyTorch model...")
         self.model.eval()
         with torch.no_grad():
             for idx, (batch_x, batch_y) in enumerate(dataloader):
@@ -73,22 +83,55 @@ class PyTorchModel(BaseModel):
                     predictions = torch.cat((predictions, outputs))
         return predictions
 
-    def evaluate(self, dataloader: DataLoader, **kwargs) -> float:
-        self.logger.info("Evaluating PyTorch model...")
+    def evaluate(self, dataloader: DataLoader, progress_prefix : str = "", **kwargs) -> float:
+        """
+        Evaluate the model on the given dataloader.
+        :param dataloader: DataLoader, containing evaluation data
+        :param progress_prefix: str, prefix for the progress logger
+        :return: total loss of dataloader
+        """
         self.model.eval()
-        loss = 0.0
+        total_loss = 0.0
+        batch_number = len(dataloader)
         with torch.no_grad():
             for idx, (batch_x, batch_y) in enumerate(dataloader):
                 batch_x, batch_y = batch_x.to(self.device), batch_y.to(self.device)
                 outputs = self.model(batch_x)
-                loss += self.criterion(outputs, batch_y).item()
-            return loss
+                loss = self.criterion(outputs, batch_y)
+                total_loss += loss.item()
+
+                criterion_losses = {"train_loss": loss.item()}
+                for name, criterion in self.additional_criterions.items():
+                    criterion_losses[name] = criterion(outputs, batch_y).item()
+                self.progress_logger.log_batch(progress_prefix, idx + 1, batch_number, criterion_losses)
+            self.progress_logger.finalize_log()
+            return total_loss
 
     def save_model(self, filepath: str) -> None:
+        """
+        Save the PyTorch model to the given filepath.
+        :param filepath: str, path to save the model
+        :return: None
+        """
+        make_parent_dir(filepath)
         self.logger.info(f"Saving PyTorch model to {filepath}...")
         torch.save(self.model.state_dict(), filepath)
 
     def load_model(self, filepath: str) -> None:
+        """
+        Load the PyTorch model from the given filepath.
+        :param filepath: str, path to load the model
+        :return: None
+        """
+        if not check_file_exist(filepath):
+            raise ValueError(f"File not found: {filepath}")
         self.logger.info(f"Loading PyTorch model from {filepath}...")
         self.model.load_state_dict(torch.load(filepath, map_location=self.device))
         self.model.eval()
+
+    def log_memory_usage(self):
+        """
+        Log the current memory usage of the GPU.
+        """
+        allocated = torch.cuda.memory_allocated(self.device) / (1024 ** 2)
+        logger.info(f"Memory allocated: {allocated:.2f} MB")
--- aidtep/ml/processor/trainer.py
+++ aidtep/ml/processor/trainer.py
@@ -1,6 +1,6 @@
 import numpy as np
+import torch
 from torch.utils.data import DataLoader
-from loguru import logger
 
 from aidtep.ml.models.torch_model import PyTorchModel
 
@@ -9,20 +9,28 @@ class Trainer:
     def __init__(self, model: PyTorchModel):
         self.model = model
 
-    def train(self, train_loader: DataLoader, val_loader: DataLoader, epochs: int):
+    def train(self, train_loader: DataLoader, val_loader: DataLoader, epochs: int,
+              model_path: str = None):
+        """
+        Train the model, save the best model based on validation loss
+        :param train_loader: DataLoader for training data
+        :param val_loader: DataLoader for validation data
+        :param epochs: int, number of epochs to train
+        :param model_path: str, path to save the best model
+        :return: None
+        """
         best_val_loss = np.inf
         train_losses = []
         val_losses = []
         for epoch in range(epochs):
-            train_loss = self.model.train(train_loader)
-            val_loss = self.model.evaluate(val_loader)
+            train_loss = self.model.train(train_loader, progress_prefix=f"Epoch {epoch+1}: ")
+            val_loss = self.model.evaluate(val_loader, progress_prefix=f"Validation: ")
             train_losses.append(train_loss)
             val_losses.append(val_loss)
-
-            logger.info(f"EPOCH: {epoch} | TRAIN LOSS: {train_loss} | VAL LOSS: {val_loss}")
-
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
-                self.model.save_model(f"model_best.pth")
+                if model_path:
+                    self.model.save_model(model_path)
+            torch.cuda.empty_cache()
 
         return train_losses, val_losses
