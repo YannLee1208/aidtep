0d693d1c9ea9f61a28df4bf98eeeb5b80b3f42d6
YannLee <chill_yann@sjtu.edu.cn>
2024-07-29 23:08:53 +0800

    update: add color in logger, set random seed in initilization

--- aidtep/utils/initialize.py
+++ aidtep/utils/initialize.py
@@ -1,7 +1,25 @@
 import logging
+import random
+import torch
+import numpy as np
 
 from aidtep.utils.logger import init_logger
 
 
-def initialize( log_level=logging.INFO):
+def set_random_seed(seed: int):
+    """
+    Set random seed for reproducibility
+    :param seed: int, random seed
+    :return:
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+
+def initialize(random_seed=0, log_level=logging.INFO):
     init_logger(log_level)
+    set_random_seed(random_seed)
--- aidtep/utils/logger.py
+++ aidtep/utils/logger.py
@@ -1,15 +1,19 @@
 import os
 import sys
 import logging
-
 from loguru import logger
 
 
-def init_logger( log_level=logging.INFO):
+def init_logger(log_level=logging.INFO):
     log_dir = "../../logs"
     logger.remove()
-    logger.add(sys.stdout, format="{time} {level} {message}", level=log_level)
+    logger.add(sys.stdout, format="<green>{time}</green> <level>{level}</level> <cyan>{message}</cyan>",
+               level=log_level, colorize=True)
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    logger.add(f"{log_dir}/file_{{time}}.log", rotation="1 day", level=log_level)
+    logger.add(f"{log_dir}/file_{{time}}.log", rotation="1 day", level=log_level, format="{time} {level} {message}")
     logger.info("Logger initialized")
+
+
+# 初始化 logger
+init_logger(logging.DEBUG)
