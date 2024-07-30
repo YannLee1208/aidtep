cea83b6aec83acf00da8786f402f0769ef5dba9a
YannLee <chill_yann@sjtu.edu.cn>
2024-07-30 23:29:04 +0800

    feature: initialize inverse problem process

--- aidtep/inverse_problem/IAEA.py
+++ aidtep/inverse_problem/IAEA.py
@@ -1,43 +1,51 @@
 import logging
-
-import torch
-from torch import nn, optim
-from torch.optim.lr_scheduler import StepLR
+from typing import Optional, Literal
+from loguru import logger
 
 from aidtep.ml.criterien.L2 import L2Loss
 from aidtep.ml.processor.trainer import Trainer
-from aidtep.ml.models.NVT_ResNet import NVT_ResNet
-from aidtep.ml.models.torch_model import PyTorchModel
+from aidtep.ml.models.base_models.torch_model import PyTorchModel
 from aidtep.ml.data.dataloader import create_dataloaders
+from aidtep.ml.utils.common import get_model_class, get_criterien, get_optimizer, get_scheduler
 from aidtep.utils.initialize import initialize
 
 
-def main_process(epoch: int = 20, lr: float = 0.005, device=None):
-    nvt_model = NVT_ResNet()
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(nvt_model.parameters(), lr=lr)
-    scheduler = StepLR(optimizer, step_size=50, gamma=0.1)
+class IAEAInverseBuilder:
+    def __init__(self):
+        pass
 
-    observation_path = "../../data/processed/IAEA/interpolation_voronoi_float16_2_mean_1_0.01_[0, 10, 20, 30, 40, 50, 60, 70, 80, 85]_[0, 10, 20, 30, 40, 50, 60, 70, 80, 85].npy"
-    output_path = "../../data/processed/IAEA/phione.npy"
-    train_ratio = 0.85
-    val_ratio = 0.1
-    batch_size = 64
-    seed = 42
-    torch.backends.cudnn.enabled = True
-    torch.backends.cudnn.benchmark = True
+    def build_dataloaders(self, x_path: str, y_path: str, train_ratio: float, val_ratio: float, batch_size: int):
+        logger.info("Building dataloaders")
+        self.train_loader, self.val_loader, self.test_loader = create_dataloaders(x_path, y_path, train_ratio,
+                                                                                  val_ratio,
+                                                                                  batch_size)
+        logger.info("Dataloaders built")
+        return self
 
-    train_loader, val_loader, test_loader = create_dataloaders(observation_path, output_path, train_ratio, val_ratio,
-                                                               batch_size, seed)
-    model = PyTorchModel(nvt_model, criterion, optimizer, scheduler, device)
-    model.add_criteria("L2", L2Loss())
+    def build_model(self, model_type: str, criterien: str, optimizer: str, scheduler: str, lr: float,
+                    device: Optional[Literal['cpu', 'cuda']]):
+        logger.info(f"Buiding model of type {model_type}, criterien {criterien}, optimizer {optimizer}, scheduler {scheduler}, lr {lr}")
+        model = get_model_class(model_type)()
+        criterion = get_criterien(criterien)()
+        optimizer = get_optimizer(optimizer)(model.parameters(), lr=lr)
+        # TODO : design a better scheduler
+        scheduler = get_scheduler(scheduler)(optimizer, step_size = 50, gamma = 0.1)
+        self.model = PyTorchModel(model, criterion, optimizer, scheduler, device)
 
-    trainer = Trainer(model)
-    trainer.train(train_loader, val_loader, epoch)
+        logger.info("Adding criterien")
+        self.model.add_criteria("L2", L2Loss())
+
+        logger.info("Model built")
+        return self
+
+    def train(self, epochs: int, model_path: str):
+        logger.info("Starting training")
+        trainer = Trainer(self.model)
+        trainer.train(self.train_loader, self.val_loader, epochs, model_path)
+        logger.info("Training done")
 
 
 if __name__ == '__main__':
     initialize(log_level=logging.DEBUG)
-    main_process()
     # import torch
     # print(torch.cuda.is_available())
