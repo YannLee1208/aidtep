a95aa57f669c5aa95820f592a08bba3b1d859107
YannLee <chill_yann@sjtu.edu.cn>
2024-09-29 13:14:50 +0800

    feature: finish svf-nn

--- aidtep/ml/models/EIM-NN.py
+++ aidtep/ml/models/EIM-NN.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch import nn
 
@@ -10,38 +11,40 @@ class EIM_NN(nn.Module, ModelRegistry):
     def name(cls):
         return "EIM-NN"
 
-    def __init__(self, Q: torch.Tensor):
+    def __init__(self, basis_path: str, x_shape: int, y_shape: int):
         """
-        :param Q: shape (basis_num, full_dim)
+        :param basis_path: path to the basis file, npz file, with key "basis". Q is the basis matrix, shape is (basis_number, full_dim)
         """
         super(EIM_NN, self).__init__()
         input_dim = 162
+        self.x_shape = x_shape
+        self.y_shape = y_shape
 
-        self.Q = Q
-        self.basis_number = Q.shape[0]
+        basis_res = np.load(basis_path)
+        self.Q = torch.tensor(basis_res["basis"])
+        self.basis_number = self.Q.shape[0]
         activation_type = nn.Tanh
 
-        self.fc = nn.Linear(input_dim, 512, dtype=Q.dtype)
+        self.fc = nn.Linear(input_dim, 512, dtype=self.Q.dtype)
         self.ac = activation_type()
-        self.fc2 = nn.Linear(512, self.basis_number, dtype=Q.dtype)
+        self.fc2 = nn.Linear(512, self.basis_number, dtype=self.Q.dtype)
 
     def forward(self, x):
+        self.Q = self.Q.to(x.device)
         output = self.ac(self.fc(x))
         output = self.fc2(output)
-        alpha = output.clone()
+        # alpha = output.clone()
 
         output = output @ self.Q
-        return output, alpha
+        output = output.view(-1, self.x_shape, self.y_shape)
+        return output
 
 
 if __name__ == "__main__":
-    import numpy as np
 
     path = "../../../data/extract_basis/NOAA/SVD_base_number_10.npz"
-    Q = np.load(path)
-    Q = torch.tensor(Q["basis"])
 
-    model = EIM_NN(Q)
+    model = EIM_NN(path)
 
     x = torch.randn(10, 162).double()
     output, alpha = model(x)
